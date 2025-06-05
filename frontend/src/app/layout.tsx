ae4f7502fccdf6b32e5da4b40bb606c2c4a4119c
abhishekblue <jainabhishekjain007@gmail.com>
2025-06-05 07:44:18 +0000

    Clerk added

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,10 +1,27 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "./components/Sidebar";
 import {Navbar} from "./components/Navbar";
+import type { Metadata } from "next";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Your App",
+  description: "Now with Clerk auth",
+};
+
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html>
+// <ClerkProvider>  
+  <html>
       <body className="bg-black text-white">
         <SidebarProvider>
           <AppSidebar />
@@ -16,5 +33,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         </SidebarProvider>
       </body>
     </html>
+// </ClerkProvider>
   );
 }
